fixed: delete some unnecessary calculation

approaches/FM3.py
@@ -309,8 +309,8 @@ def _FM3(
         pos += delta_pos * drag_arr * pinning
         # cool temperature
         t -= dt
-        if (np.linalg.norm(delta_pos) / nnodes) < threshold:
-            break
+        # if (np.linalg.norm(delta_pos) / nnodes) < threshold:
+        #     break
         frac_done += 1.0 / float(iteration + 1)
 
     return pos
@@ -388,6 +388,6 @@ def _sparse_FM3(
         pos += delta_pos * drag_arr
         # cool temperature
         t -= dt
-        if (np.linalg.norm(delta_pos) / nnodes) < threshold:
-            break
+        # if (np.linalg.norm(delta_pos) / nnodes) < threshold:
+        #     break
     return pos

approaches/FR.py
@@ -194,6 +194,7 @@ def fr_layout(
         dom_size = 1
 
     drag_arr = None
+    print(drag_index)
     if drag_index is not None:
         drag_arr=[]
         if isinstance(drag_index, dict):
@@ -308,8 +309,8 @@ def _fruchterman_reingold(
         pos += delta_pos * drag_arr * pinning
         # cool temperature
         t -= dt
-        if (np.linalg.norm(delta_pos) / nnodes) < threshold:
-            break
+        # if (np.linalg.norm(delta_pos) / nnodes) < threshold:
+        #     break
         frac_done += 1.0 / float(iteration + 1)
 
     return pos
@@ -387,6 +388,6 @@ def _sparse_fruchterman_reingold(
         pos += delta_pos * drag_arr
         # cool temperature
         t -= dt
-        if (np.linalg.norm(delta_pos) / nnodes) < threshold:
-            break
+        # if (np.linalg.norm(delta_pos) / nnodes) < threshold:
+        #     break
     return pos